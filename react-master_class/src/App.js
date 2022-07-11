cac232f9a34856860a7d4a07435e91e5cc8de2cb
oddcode <oddcode@naver.com>
2022-07-12 02:42:35 +0900

    Use Props

--- react-master_class/src/App.js
+++ react-master_class/src/App.js
@@ -1,30 +1,17 @@
 import styled from "styled-components";
 
-const Father = styled.div`
-  display: flex;
-`;
-const BoxOne = styled.div`
-  background-color: teal;
+const Alias = styled.div`
+  background-color: ${(props) => props.color};
   width: 100px;
   height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.span`
-  color: white;
 `;
 
 function App() {
   return (
-    <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
-    </Father>
+    <div>
+      <Alias color="blue" />
+      <Alias color="red" />
+    </div>
   );
 }
 
